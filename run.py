e701f745b508acaa590f64622e264ad21a9b4e7a
Vagus-art <agustin.favo@gmail.com>
2020-11-22 15:09:40 -0300

    first commit

---
+++ run.py
@@ -0,0 +1,46 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
+
+def rootHandler(base : BaseHTTPRequestHandler):
+    base.send_header("Content-type","text")
+    base.end_headers
+    base.wfile.write("hello!".encode())
+
+def echoHandler(base : BaseHTTPRequestHandler):
+    '''
+    parses request args and echoes them separated by new lines
+    '''
+    base.send_header("Content-type","text")
+    base.end_headers
+    args = [arg for arg in base.path.split("?")[1].split('&')]
+    base.wfile.write('\n'.join(args).encode())
+
+routes = [{
+    'path': '/',
+    'handler': rootHandler
+},
+{
+    'path': '/echo',
+    'handler': echoHandler
+}]
+
+class helloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        found = False
+        for route in routes:
+            if route['path'] == urlparse(self.path).path:
+                route['handler'](self)
+                found = True
+        if found == False:
+            self.wfile.write(f"{self.path} not found!".encode())
+
+
+def main():
+    PORT = 3000
+    server = HTTPServer(('',PORT),helloHandler)
+    print("server running!")
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
