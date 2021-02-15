e1c3777428cb28b8e49d3d370c75e62ddf2ceba5
Vagus-art <agustin.favo@gmail.com>
2021-02-15 17:48:29 -0300

    add primes hmk

---
+++ hmk/11.py
@@ -0,0 +1,19 @@
+""" Ask the user for a number and determine whether the number is prime or not.
+(For those who have forgotten, a prime number is a number that has no divisors.).
+ You can (and should!) use your answer to Exercise 4 to help you.
+  Take this opportunity to practice using functions, described below. """
+
+def is_prime(number):
+    for i in range(2,number):
+        if number%i==0:
+            return False
+    return True
+
+print("insert a number:")
+
+number = int(input())
+
+if is_prime(number):
+    print("is prime")
+else:
+    print("not prime")
